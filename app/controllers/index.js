9a9ff1f5f04fc39a474530af1b156e525a0faeff
Perrywzp <wangzhipei1121@163.com>
2016-07-31 18:49:09 +0800

    [增加功能]增加分页,搜索

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -12,7 +12,7 @@ exports.index = function (req, res) {
   Category
     .find({})
     .populate({path: 'movies', options: {limit: 5}})
-    .exec(function(err, categories){
+    .exec(function (err, categories) {
       if (err) {
         console.log(err);
       }
@@ -22,4 +22,61 @@ exports.index = function (req, res) {
       });
     });
 
+};
+
+// 搜索功能
+exports.search = function (req, res) {
+  var catId = req.query.cat;
+  var q = req.query.q;
+  var page = parseInt(req.query.p, 10) || 0;
+  var count = 2;
+  var index = page * count;
+
+  if (catId) {
+    Category
+      .find({_id: catId})
+      .populate({
+        path: 'movies',
+        select: 'title poster'
+        // ,options: {limit: 2, skip: index} //mongo自带的分页方法有问题
+      })
+      .exec(function (err, categories) {
+        if (err) {
+          console.log(err);
+        }
+
+        var category = categories[0] || {};
+        var movies = category.movies || [];
+        var results = movies.slice(index, index + count);
+
+        res.render('results', {
+          title: 'movie 结果列表页',
+          keyword: category.name,
+          currentPage: page + 1,
+          query: 'cat=' + catId,
+          totalPage: Math.ceil(movies.length / count),
+          movies: results
+        });
+      });
+  }
+  else {
+    Movie
+      .find({title: new RegExp(q + '.*', 'i')}) //通过加正则,实现模糊匹配
+      .exec(function (err, movies) {
+        if (err) {
+          console.log(err);
+        }
+
+        var results = movies.slice(index, index + count);
+
+        res.render('results', {
+          title: 'movie 结果列表页',
+          keyword: q,
+          currentPage: page + 1,
+          query: 'q=' + q,
+          totalPage: Math.ceil(movies.length / count),
+          movies: results
+        });
+      });
+  }
 };
